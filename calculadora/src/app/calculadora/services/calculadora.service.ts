6dbf80a7e731b73a0c403bf12e4f651387a65635
willianpilar <willianpilar@hotmail.com>
2022-01-03 21:18:20 -0300

    Implementando o service da calculadora

--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -5,9 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly ADICAO : string = '+';
+  static readonly SUBTRACAO : string = '-';
+  static readonly MULTIPLICACAO : string = '*';
+  static readonly DIVISAO : string = '/';
+
   constructor() { }
 
-  Teste(){
-    return "Teste";
+  calcular( num1 : number, num2 : number, operacao : string ) : number{
+    
+    let resultado : number;
+
+    switch(operacao){
+      case CalculadoraService.ADICAO:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;     
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+
   }
 }
