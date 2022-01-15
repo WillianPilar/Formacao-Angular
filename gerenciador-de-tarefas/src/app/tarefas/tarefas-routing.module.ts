90104843f3db734c2d7ba1eec07aad1d1b60c60f
willianpilar <willianpilar@hotmail.com>
2022-01-15 19:09:45 -0300

    Incluindo componente de cadastro de tarefa

--- gerenciador-de-tarefas/src/app/tarefas/tarefas-routing.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CadastrarTarefaComponent } from "./cadastrar/cadastrar-tarefa.component";
 import { ListarTarefasComponent } from "./listar/listar-tarefas.component";
 
 export const routes: Routes = [
-    {
-        path: 'tarefas', redirectTo: 'tarefas/listar'
-    },
-    {
-        path: 'tarefas/listar', component: ListarTarefasComponent
-    }
+    { path: 'tarefas', redirectTo: 'tarefas/listar' },
+    { path: 'tarefas/listar', component: ListarTarefasComponent },
+    { path: 'tarefas/cadastrar', component: CadastrarTarefaComponent }
 ];
 
 @NgModule({
--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -5,12 +5,14 @@ import { ListarTarefasComponent } from './listar/listar-tarefas.component';
 import { TarefasRoutingModule } from './tarefas-routing.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar/cadastrar-tarefa.component';
 
 
 
 @NgModule({
   declarations: [
-    ListarTarefasComponent
+    ListarTarefasComponent,
+    CadastrarTarefaComponent
   ],
   imports: [
     CommonModule,
